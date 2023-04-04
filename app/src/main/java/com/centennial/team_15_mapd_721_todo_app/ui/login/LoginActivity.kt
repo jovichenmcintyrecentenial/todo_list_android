c529ce1cc3a70a82ac252cf9fcae247991c4972f
jovi848 <j.mcintyre@senit.com>
2023-04-04 01:56:16 -0400

    start sign up UI

--- app/src/main/java/com/centennial/team_15_mapd_721_todo_app/ui/login/LoginActivity.kt
+++ app/src/main/java/com/centennial/team_15_mapd_721_todo_app/ui/login/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.centennial.team_15_mapd_721_todo_app.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.centennial.team_15_mapd_721_todo_app.R
 import com.centennial.team_15_mapd_721_todo_app.models.Database
+import com.centennial.team_15_mapd_721_todo_app.ui.signup.SignUpActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +14,11 @@ class LoginActivity : AppCompatActivity() {
         Database.initDatabase(applicationContext)
 
         setContentView(R.layout.activity_login)
+    }
+
+    fun onCreateAccountClicked(view: View) {
+
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/centennial/team_15_mapd_721_todo_app/ui/signup/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.centennial.team_15_mapd_721_todo_app.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.centennial.team_15_mapd_721_todo_app.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
